b56807411431c5bf86a03f902d87223721f7c9aa
Emmanuel Adedeji <emmans4destiny@gmail.com>
2025-12-15 17:27:12 +0200

    refactor

--- main_dashboard.py
+++ main_dashboard.py
@@ -15,7 +15,7 @@ from data_loader import load_live_data_from_gsheet, fetch_historical_data, fetch
 from refresh_utils import check_auto_refresh_conditions
 
 st.set_page_config(layout="wide")
-st.title("📈 My Nigerian Stock Exchange Portfolio Dashboard")
+st.title("📈 NGX Portfolio Dashboard")
 
 market_status = fetch_market_status()
 st.subheader(f"Market Status: {market_status}")
